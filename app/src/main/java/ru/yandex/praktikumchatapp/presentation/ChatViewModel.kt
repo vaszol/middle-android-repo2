a1650da436bb7911b2d9eba6ba38c62e2ed31d5f
vasiliy <vaszol@mail.ru>
2025-01-24 03:55:29 +0300

    Задание 1 заменить использование LiveData на StateFlow

--- app/src/main/java/ru/yandex/praktikumchatapp/presentation/ChatViewModel.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/presentation/ChatViewModel.kt
@@ -1,9 +1,10 @@
 package ru.yandex.praktikumchatapp.presentation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import ru.yandex.praktikumchatapp.data.ChatRepository
 
@@ -13,17 +14,16 @@ class ChatViewModel(
 
     private val repository = ChatRepository()
 
-    private val _messages = MutableLiveData<List<Message>>(emptyList())
-    val messages: LiveData<List<Message>> = _messages
+    private val _messages = MutableStateFlow(emptyList<Message>())
+    val messages = _messages.asStateFlow()
 
     init {
         viewModelScope.launch {
             while (isWithReplies) {
                 repository.getReplyMessage().collect { response ->
-
-                    val currentMessages = _messages.value ?: emptyList()
-                    _messages.value =
-                        currentMessages + Message.OtherMessage(response)
+                    _messages.update {
+                        it.plusElement(Message.OtherMessage(response))
+                    }
 
                 }
             }
@@ -31,7 +31,8 @@ class ChatViewModel(
     }
 
     fun sendMyMessage(messageText: String) {
-        val currentMessages = _messages.value ?: emptyList()
-        _messages.value = currentMessages + Message.MyMessage(messageText)
+        _messages.update {
+            it.plusElement(Message.MyMessage(messageText))
+        }
     }
 }
