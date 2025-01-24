00cca6b938e8189336a484ab6fe7940fc92ff54c
vasiliy <vaszol@mail.ru>
2025-01-24 04:38:38 +0300

    Задание 2 написать unit-тест наличия отправленного сообщения

--- app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
+++ app/src/test/java/ru/yandex/praktikumchatapp/ChatViewModelTest.kt
@@ -32,7 +32,8 @@ class ChatViewModelTest {
     @Test
     fun `send message should update messages with MyMessage`() = runTest {
         val message = Message.MyMessage("TestMessage")
-
+        viewModel.sendMyMessage(message.text)
+        assert(viewModel.messages.value.contains(message))
     }
 
     @Test
