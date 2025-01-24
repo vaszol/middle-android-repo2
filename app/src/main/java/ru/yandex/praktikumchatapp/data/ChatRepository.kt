f4748461f9480c3f07dfec37574133cae3a0298d
vasiliy <vaszol@mail.ru>
2025-01-24 19:05:30 +0300

    Задание 4 написать логику обработки ошибок «сети»

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,28 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
+import java.io.IOException
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
 
     fun getReplyMessage(): Flow<String> {
-        return api.getReply()
+        return api.getReply().retryWhen { cause, attempt ->
+            if (cause is IOException || attempt < 5) {
+                delay(currentDelay)
+                currentDelay *= DELAY_FACTOR
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private companion object {
+        var currentDelay = 1000L
+        const val DELAY_FACTOR = 2
     }
 }
